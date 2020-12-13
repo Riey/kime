49ed43397326698a33ea2f4554534ac26389fac4
Riey <creeper844@gmail.com>
2020-12-13 17:11:13 +0900

    Impl Open

--- xim/src/types.rs
+++ xim/src/types.rs
@@ -1,26 +1,145 @@
+use crate::reader::{ReadError, Readable, Reader, Result};
+use crate::writer::Writable;
 use enumflags2::BitFlags;
 use num_derive::FromPrimitive;
+use std::marker::PhantomData;
 
 pub type WINDOW = u32;
 
-#[derive(Eq, PartialEq, Clone, Debug)]
-pub enum Request<'a> {
-    Connect(Connect<'a>),
-    ConnectReply(ConnectReply),
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub struct RequestHeader {
+    pub major_opcode: Opcode,
+    pub minor_opcode: u8,
+    pub size: u16,
 }
 
-impl<'a> Request<'a> {
-    pub fn opcode(&self) -> Opcode {
-        match self {
-            Request::Connect(..) => Opcode::Connect,
-            Request::ConnectReply(..) => Opcode::ConnectReply,
+macro_rules! define_request {
+    (
+        $(
+            $op:ident = $n:literal,
+        )+
+    ) => {
+        #[derive(Eq, PartialEq, Copy, Clone, Debug)]
+        pub enum Opcode {
+            $($op = $n,)+
         }
-    }
+
+        #[derive(Eq, PartialEq, Clone, Debug)]
+        pub enum Request<'a> {
+            $($op($op<'a>),)+
+        }
+
+        impl<'a> Request<'a> {
+            fn content_size(&self) -> usize {
+                match self {
+                    $(Request::$op(req) => req.size(),)+
+                }
+            }
+
+            fn opcode(&self) -> Opcode {
+                match self {
+                    $(Request::$op(req) => Opcode::$op,)+
+                }
+            }
+        }
+
+        impl<'a> Readable<'a> for Opcode {
+            fn read(reader: &mut Reader<'a>) -> Result<Self> {
+                match reader.u8()? {
+                    $($n => Ok(Opcode::$op),)+
+                    _ => Err(ReadError::InvalidData),
+                }
+            }
+        }
+
+        impl Writable for Opcode {
+            fn write(&self, out: &mut Vec<u8>) {
+                out.push(*self as u8);
+            }
+
+            fn size(&self) -> usize {
+                1
+            }
+        }
+
+        impl<'a> Readable<'a> for Request<'a> {
+            fn read(reader: &mut Reader<'a>) -> Result<Self> {
+                let header = RequestHeader::read(reader)?;
+
+                match header.major_opcode {
+                    $(Opcode::$op => Ok(Request::$op($op::read(reader)?)),)+
+                }
+            }
+        }
+
+        impl<'a> Writable for Request<'a> {
+            fn write(&self, out: &mut Vec<u8>) {
+                let header = RequestHeader {
+                    major_opcode: self.opcode(),
+                    minor_opcode: 0,
+                    size: (self.content_size() / 4) as u16,
+                };
+
+                header.write(out);
+
+                match self {
+                    $(Request::$op(req) => req.write(out),)+
+                }
+            }
+
+            fn size(&self) -> usize {
+                self.content_size() + 4
+            }
+        }
+    };
+}
+
+define_request! {
+    Connect = 1,
+    ConnectReply = 2,
+    // Disconnect = 3,
+    // DisconnectReply = 4,
+    //
+    Open = 30,
+    OpenReply = 31,
+    // Close = 32,
+    // CloseReply = 33,
+    // RegisterTriggerKeys = 34,
+    // TriggerNotifyReply = 36,
+    //
+    // SetEventMask = 37,
+    // EncodingNegotiationReply = 39,
+    // QueryExtensionReply = 41,
+    // SetImValuesReply = 43,
+    // GetImValuesReply = 45,
+    //
+    // CreateIcReply = 51,
+    // DestroyIcReply = 53,
+    // SetIcValuesReply = 55,
+    // GetIcValuesReply = 57,
+    // SyncReply = 62,
+    // Commit = 63,
+    // ResetIcReply = 65,
+    //
+    // Geometry = 70,
+    // StrConversion = 71,
+    // PreeditStart = 73,
+    // PreeditDraw = 75,
+    // PreeditCaret = 76,
+    // PreeditDone = 78,
+    // StatusStart = 79,
+    // StatusDraw = 80,
+    // StatusDone = 81,
+    // PreeditState = 82,
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
 #[repr(transparent)]
-pub struct XimString<'a>(pub &'a str);
+pub struct XimString<'a>(pub &'a [u8]);
+
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+#[repr(transparent)]
+pub struct XimStr<'a>(pub &'a [u8]);
 
 #[derive(Eq, PartialEq, Clone, Debug)]
 pub struct Connect<'a> {
@@ -30,55 +149,22 @@ pub struct Connect<'a> {
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
-pub struct ConnectReply {
+pub struct ConnectReply<'a> {
     pub server_major_protocol_version: u16,
     pub server_minor_protocol_version: u16,
+    pub _marker: PhantomData<&'a ()>,
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
-pub struct RequestHeader {
-    pub major_opcode: Opcode,
-    pub minor_opcode: u8,
-    pub length: u16,
+pub struct Open<'a> {
+    pub name: XimStr<'a>,
 }
 
-#[derive(Eq, PartialEq, Copy, Clone, Debug, FromPrimitive)]
-#[repr(u8)]
-pub enum Opcode {
-    Connect = 1,
-    ConnectReply = 2,
-    Disconnect = 3,
-    DisconnectReply = 4,
-
-    OpenReply = 31,
-    CloseReply = 33,
-    RegisterTriggerKeys = 34,
-    TriggerNotifyReply = 36,
-
-    SetEventMask = 37,
-    EncodingNegotiationReply = 39,
-    QueryExtensionReply = 41,
-    SetImValuesReply = 43,
-    GetImValuesReply = 45,
-
-    CreateIcReply = 51,
-    DestroyIcReply = 53,
-    SetIcValuesReply = 55,
-    GetIcValuesReply = 57,
-    SyncReply = 62,
-    Commit = 63,
-    ResetIcReply = 65,
-
-    Geometry = 70,
-    StrConversion = 71,
-    PreeditStart = 73,
-    PreeditDraw = 75,
-    PreeditCaret = 76,
-    PreeditDone = 78,
-    StatusStart = 79,
-    StatusDraw = 80,
-    StatusDone = 81,
-    PreeditState = 82,
+#[derive(Eq, PartialEq, Clone, Debug)]
+pub struct OpenReply<'a> {
+    pub input_method_id: u16,
+    pub xim_attributes: Vec<Attr<'a>>,
+    pub xic_attributes: Vec<Attr<'a>>,
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
--- xim/src/writer.rs
+++ xim/src/writer.rs
@@ -2,4 +2,5 @@ use crate::types::*;
 
 pub trait Writable {
     fn write(&self, out: &mut Vec<u8>);
+    fn size(&self) -> usize;
 }

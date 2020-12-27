959751d470954b8ed149aeb05f679795dbbf8af5
Riey <creeper844@gmail.com>
2020-12-27 14:26:08 +0900

    Initial support for compose moum

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), ServerError> {
     pretty_env_logger::init();
     let (conn, screen_num) = x11rb::rust_connection::RustConnection::connect(None)?;
     let screen = &conn.setup().roots[screen_num];
-    let mut server = xim::x11rb::X11rbServer::init(&conn, screen, "test_server")?;
+    let mut server = xim::x11rb::X11rbServer::init(&conn, screen, "kime")?;
     let mut connections = XimConnections::new();
     let mut handler = self::handler::KimeHandler::new();
 

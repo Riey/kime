9f0ffba180051018c2d58c2d8d73a4a8ff1039f6
Riey <creeper844@gmail.com>
2021-01-07 16:13:14 +0900

    Pass com_moum

--- src/engine/state.rs
+++ src/engine/state.rs
@@ -1,82 +1,165 @@
-use super::characters::{Choseong, Jongseong, Jungseong};
+use super::characters::{Choseong, JongToCho, Jongseong, Jungseong};
 use super::InputResult;
-use std::mem;
 
-#[derive(Debug, Clone, Copy)]
-pub enum CharacterState {
-    Empty,
-    Cho(Choseong),
-    Jung(Jungseong),
-    ChoJung(Choseong, Jungseong),
-    ChoJungJong(Choseong, Jungseong, Jongseong),
-}
-
-impl Default for CharacterState {
-    fn default() -> Self {
-        CharacterState::Empty
-    }
+#[derive(Debug, Default, Clone, Copy)]
+pub struct CharacterState {
+    cho: Option<Choseong>,
+    jung: Option<Jungseong>,
+    jong: Option<Jongseong>,
 }
 
 impl CharacterState {
-    fn to_char(&self) -> char {
-        match self {
-            CharacterState::Empty => '\0',
-            CharacterState::Cho(cho) => cho.jamo(),
-            CharacterState::Jung(jung) => jung.jamo(),
-            CharacterState::ChoJung(cho, jung) => cho.compose(*jung, None),
-            CharacterState::ChoJungJong(cho, jung, jong) => cho.compose(*jung, Some(*jong)),
+    pub fn to_char(&self) -> char {
+        match (self.cho, self.jung, self.jong) {
+            (None, None, None) => '\0',
+            (Some(cho), None, None) => cho.jamo(),
+            (None, Some(jung), None) => jung.jamo(),
+            (None, None, Some(jong)) => jong.jamo(),
+            (None, Some(_jung), Some(_jong)) => '\0',
+            (Some(cho), None, Some(_jong)) => cho.jamo(),
+            (Some(cho), Some(jung), jong) => cho.compose(jung, jong),
         }
     }
 
+    /// Replace self with new then return previous status char
+    fn replace(&mut self, new: Self) -> char {
+        let prev = std::mem::replace(self, new);
+        prev.to_char()
+    }
+
+    // 두벌식용
     pub fn cho_jong(&mut self, cho: Choseong, jong: Jongseong) -> InputResult {
-        if matches!(
-            self,
-            CharacterState::ChoJung(..) | CharacterState::ChoJungJong(..)
-        ) {
-            self.jong(jong)
-        } else {
+        if self.jung.is_none() {
             self.cho(cho)
+        } else {
+            self.jong(jong)
         }
     }
 
     pub fn cho(&mut self, cho: Choseong) -> InputResult {
-        match *self {
-            CharacterState::Empty => {
-                *self = CharacterState::Cho(cho);
-                InputResult::Preedit(cho.jamo())
+        if let Some(prev_cho) = self.cho {
+            match prev_cho.try_add(cho) {
+                Some(new) => {
+                    self.cho = Some(new);
+                    InputResult::Preedit(self.to_char())
+                }
+                None => {
+                    let commit = self.replace(Self {
+                        cho: Some(cho),
+                        ..Default::default()
+                    });
+                    InputResult::CommitPreedit(commit, self.to_char())
+                }
             }
-            CharacterState::Cho(prev_cho) => match prev_cho {
-                Choseong::Giyeok => {
-                    *self = CharacterState::Cho(Choseong::SsangGiyeok);
-                    InputResult::Preedit(Choseong::Giyeok.jamo())
-                }
-                _ => {
-                    *self = CharacterState::Cho(cho);
-                    InputResult::CommitPreedit(prev_cho.jamo(), cho.jamo())
-                }
-            },
-            _ => todo!(),
+        } else {
+            self.cho = Some(cho);
+            InputResult::Preedit(self.to_char())
         }
     }
 
     pub fn jung(&mut self, jung: Jungseong) -> InputResult {
-        match *self {
-            CharacterState::Empty => {
-                *self = CharacterState::Jung(jung);
-                InputResult::Preedit(jung.jamo())
+        // TODO: try decompose jongseong
+        if let Some(prev_jung) = self.jung {
+            match prev_jung.try_add(jung) {
+                Some(new) => {
+                    self.jung = Some(new);
+                    InputResult::Preedit(self.to_char())
+                }
+                None => {
+                    let new;
+
+                    if let Some(jong) = self.jong {
+                        match jong.to_cho() {
+                            JongToCho::Direct(cho) => {
+                                self.jong = None;
+                                new = Self {
+                                    cho: Some(cho),
+                                    jung: Some(jung),
+                                    ..Default::default()
+                                };
+                            }
+                            JongToCho::Compose(left, cho) => {
+                                self.jong = Some(left);
+                                new = Self {
+                                    cho: Some(cho),
+                                    jung: Some(jung),
+                                    ..Default::default()
+                                };
+                            }
+                        }
+                    } else {
+                        new = Self {
+                            jung: Some(jung),
+                            ..Default::default()
+                        };
+                    }
+
+                    let commit = self.replace(new);
+                    InputResult::CommitPreedit(commit, self.to_char())
+                }
             }
-            CharacterState::Cho(cho) => {
-                *self = CharacterState::ChoJung(cho, jung);
-                InputResult::Preedit(cho.compose(jung, None))
-            }
-            CharacterState::Jung(..) | CharacterState::ChoJung(..) => todo!("compose jungseong"),
-            CharacterState::ChoJungJong(cho, prev_jung, jong) => {
-                todo!("split jongseong")
-            }
+        } else {
+            self.jung = Some(jung);
+            InputResult::Preedit(self.to_char())
         }
     }
 
     pub fn jong(&mut self, jong: Jongseong) -> InputResult {
-        todo!()
+        if let Some(prev_jong) = self.jong {
+            match prev_jong.try_add(jong) {
+                Some(new) => {
+                    self.jong = Some(new);
+                    InputResult::Preedit(self.to_char())
+                }
+                None => {
+                    let new;
+
+                    match prev_jong.to_cho() {
+                        JongToCho::Direct(cho) => {
+                            new = Self {
+                                cho: Some(cho),
+                                ..Default::default()
+                            };
+                        }
+                        JongToCho::Compose(..) => {
+                            new = Self {
+                                jong: Some(jong),
+                                ..Default::default()
+                            };
+                        }
+                    }
+
+                    let commit = self.replace(new);
+                    InputResult::CommitPreedit(commit, self.to_char())
+                }
+            }
+        } else {
+            self.jong = Some(jong);
+            InputResult::Preedit(self.to_char())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn jong() {
+        let mut state = CharacterState::default();
+
+        assert_eq!(
+            InputResult::Preedit('ㅇ'),
+            state.cho_jong(Choseong::Ieung, Jongseong::Ieung)
+        );
+        assert_eq!(InputResult::Preedit('아'), state.jung(Jungseong::A));
+        assert_eq!(
+            InputResult::Preedit('앙'),
+            state.cho_jong(Choseong::Ieung, Jongseong::Ieung)
+        );
+        assert_eq!(
+            InputResult::CommitPreedit('아', '아'),
+            state.jung(Jungseong::A)
+        );
     }
 }

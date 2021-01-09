d98c2b18f19ce07f7e263ef14f766e1470d1fe6a
Riey <creeper844@gmail.com>
2021-01-09 20:03:35 +0900

    Integrate xkbcommon in engine

--- engine/src/dubeolsik.rs
+++ engine/src/dubeolsik.rs
@@ -2,106 +2,96 @@ pub const DUBEOLSIK_LAYOUT: &str = include_str!("../data/dubeolsik.yaml");
 
 #[cfg(test)]
 mod tests {
-    use super::super::{
-        state::CharacterState,
-        InputResult,
-        KeyCode::{self, *},
-        Layout,
-    };
+    use super::super::{state::CharacterState, InputResult, Layout};
+
+    use xkbcommon::xkb::*;
 
     use pretty_assertions::assert_eq;
 
     #[track_caller]
-    fn test_input(inputs: &[(KeyCode, InputResult)]) {
+    fn test_input(inputs: &[(Keysym, InputResult)]) {
         let layout = Layout::dubeolsik();
-        let mut enable_hangul = true;
         let mut state = CharacterState::default();
 
         for (code, expect_result) in inputs.iter().copied() {
-            assert_eq!(
-                expect_result,
-                layout.map_key(&mut state, &mut enable_hangul, code, false)
-            );
+            assert_eq!(expect_result, layout.map_key(&mut state, code));
         }
     }
 
     #[test]
     fn com_moum() {
         test_input(&[
-            (D, InputResult::Preedit('ㅇ')),
-            (H, InputResult::Preedit('오')),
-            (L, InputResult::Preedit('외')),
-            (D, InputResult::Preedit('욍')),
-            (D, InputResult::CommitPreedit('욍', 'ㅇ')),
-            (K, InputResult::Preedit('아')),
-            (S, InputResult::Preedit('안')),
-            (G, InputResult::Preedit('않')),
-            (E, InputResult::CommitPreedit('않', 'ㄷ')),
+            (KEY_d, InputResult::Preedit('ㅇ')),
+            (KEY_h, InputResult::Preedit('오')),
+            (KEY_l, InputResult::Preedit('외')),
+            (KEY_d, InputResult::Preedit('욍')),
+            (KEY_d, InputResult::CommitPreedit('욍', 'ㅇ')),
+            (KEY_k, InputResult::Preedit('아')),
+            (KEY_s, InputResult::Preedit('안')),
+            (KEY_g, InputResult::Preedit('않')),
+            (KEY_e, InputResult::CommitPreedit('않', 'ㄷ')),
         ]);
     }
 
     #[test]
     fn number() {
         test_input(&[
-            (D, InputResult::Preedit('ㅇ')),
-            (H, InputResult::Preedit('오')),
-            (L, InputResult::Preedit('외')),
-            (D, InputResult::Preedit('욍')),
-            (D, InputResult::CommitPreedit('욍', 'ㅇ')),
-            (K, InputResult::Preedit('아')),
-            (S, InputResult::Preedit('안')),
-            (G, InputResult::Preedit('않')),
-            (E, InputResult::CommitPreedit('않', 'ㄷ')),
-            (One, InputResult::CommitCommit('ㄷ', '1')),
+            (KEY_d, InputResult::Preedit('ㅇ')),
+            (KEY_h, InputResult::Preedit('오')),
+            (KEY_l, InputResult::Preedit('외')),
+            (KEY_d, InputResult::Preedit('욍')),
+            (KEY_d, InputResult::CommitPreedit('욍', 'ㅇ')),
+            (KEY_k, InputResult::Preedit('아')),
+            (KEY_s, InputResult::Preedit('안')),
+            (KEY_g, InputResult::Preedit('않')),
+            (KEY_e, InputResult::CommitPreedit('않', 'ㄷ')),
+            (KEY_1, InputResult::CommitCommit('ㄷ', '1')),
         ]);
     }
 
     #[test]
     fn exclamation_mark() {
-        let mut state = CharacterState::default();
-        let layout = Layout::dubeolsik();
-
-        assert_eq!(
-            layout.map_key(&mut state, &mut true, One, true),
-            InputResult::Commit('!')
-        );
+        test_input(&[
+            (KEY_R, InputResult::Preedit('ㄲ')),
+            (KEY_exclam, InputResult::CommitCommit('ㄲ', '!')),
+        ]);
     }
 
     #[test]
     fn backspace() {
         test_input(&[
-            (R, InputResult::Preedit('ㄱ')),
-            (K, InputResult::Preedit('가')),
-            (D, InputResult::Preedit('강')),
-            (Bs, InputResult::Preedit('가')),
-            (Q, InputResult::Preedit('갑')),
-            (T, InputResult::Preedit('값')),
-            (Bs, InputResult::Preedit('갑')),
-            (Bs, InputResult::Preedit('가')),
-            (Bs, InputResult::Preedit('ㄱ')),
-            (Bs, InputResult::ClearPreedit),
-            (R, InputResult::Preedit('ㄱ')),
+            (KEY_r, InputResult::Preedit('ㄱ')),
+            (KEY_k, InputResult::Preedit('가')),
+            (KEY_d, InputResult::Preedit('강')),
+            (KEY_BackSpace, InputResult::Preedit('가')),
+            (KEY_q, InputResult::Preedit('갑')),
+            (KEY_t, InputResult::Preedit('값')),
+            (KEY_BackSpace, InputResult::Preedit('갑')),
+            (KEY_BackSpace, InputResult::Preedit('가')),
+            (KEY_BackSpace, InputResult::Preedit('ㄱ')),
+            (KEY_BackSpace, InputResult::ClearPreedit),
+            (KEY_r, InputResult::Preedit('ㄱ')),
         ])
     }
 
     #[test]
     fn compose_jong() {
         test_input(&[
-            (D, InputResult::Preedit('ㅇ')),
-            (J, InputResult::Preedit('어')),
-            (Q, InputResult::Preedit('업')),
-            (T, InputResult::Preedit('없')),
+            (KEY_d, InputResult::Preedit('ㅇ')),
+            (KEY_j, InputResult::Preedit('어')),
+            (KEY_q, InputResult::Preedit('업')),
+            (KEY_t, InputResult::Preedit('없')),
         ])
     }
 
     #[test]
     fn backspace_moum_compose() {
         test_input(&[
-            (D, InputResult::Preedit('ㅇ')),
-            (H, InputResult::Preedit('오')),
-            (K, InputResult::Preedit('와')),
-            (Bs, InputResult::Preedit('오')),
-            (Bs, InputResult::Preedit('ㅇ')),
+            (KEY_d, InputResult::Preedit('ㅇ')),
+            (KEY_h, InputResult::Preedit('오')),
+            (KEY_k, InputResult::Preedit('와')),
+            (KEY_BackSpace, InputResult::Preedit('오')),
+            (KEY_BackSpace, InputResult::Preedit('ㅇ')),
         ])
     }
 }

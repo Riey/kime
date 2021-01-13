01ca2cc18967767337ed967f850714f6ec4d8ae6
Riey <creeper844@gmail.com>
2021-01-13 16:07:29 +0900

    Add systemd service file

--- release.sh
+++ release.sh
@@ -6,6 +6,8 @@ mkdir -pv build/layout
 cp target/release/kime-xim build/kime-xim
 cp target/release/libkime_gtk3.so build/im-kime.so
 
+cp kime-xim.service build/kime-xim.service
+
 cp engine/data/dubeolsik.yaml build/layout
 cp engine/data/sebeolsik-390.yaml build/layout
 cp engine/data/sebeolsik-391.yaml build/layout

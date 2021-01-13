1881babb2ea3ad228b4ca6d61757411f7423f243
Riey <creeper844@gmail.com>
2021-01-13 16:16:15 +0900

    Rename release layout folder to layouts

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 cargo build --release
 
 mkdir -pv build
-mkdir -pv build/layout
+mkdir -pv build/layouts
 
 cp target/release/kime-xim build/kime-xim
 cp target/release/libkime_gtk3.so build/im-kime.so
 
 cp kime-xim.service build/kime-xim.service
 
-cp engine/data/dubeolsik.yaml build/layout
-cp engine/data/sebeolsik-390.yaml build/layout
-cp engine/data/sebeolsik-391.yaml build/layout
+cp engine/data/dubeolsik.yaml build/layouts
+cp engine/data/sebeolsik-390.yaml build/layouts
+cp engine/data/sebeolsik-391.yaml build/layouts
 
 cp engine/data/config.yaml build/
 

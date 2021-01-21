727503e13624419fa7444c1027f43ce084294e34
Riey <creeper844@gmail.com>
2021-01-21 18:59:26 +0900

    Don't use cargo run on release script

--- pkg/release.sh
+++ pkg/release.sh
@@ -12,7 +12,7 @@ cp target/release/kime-xim build/out/kime-xim
 cp target/release/libkime_gtk3.so build/out/im-kime.so
 cp target/release/libkime_engine.so build/out/libkime_engine.so
 
-cargo run --release -p kime-engine-config-writer > build/out/config.yaml
+target/release/kime-engine-config-writer > build/out/config.yaml
 cp engine/cffi/kime_engine.h build/out
 
 strip -s build/out/kime-xim

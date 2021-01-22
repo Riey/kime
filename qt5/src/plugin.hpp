44d064c13a1f1d6d150301c7de22bdd15cea1dc7
Riey <creeper844@gmail.com>
2021-01-23 00:39:05 +0900

    Implement QtPlugin

--- qt5/src/plugin.hpp
+++ qt5/src/plugin.hpp
@@ -1,11 +1,16 @@
-#include <QtCore/QtGlobal>
+#include <QtPlugin>
 #include <QString>
 #include <QInputMethodEvent>
 #include <qpa/qplatforminputcontextplugin_p.h>
 
-#include <kime/kime_engine.h>
+extern "C" {
+    #include <kime/kime_engine.h>
+}
 
 class KimePlatformInputContextPlugin: public QPlatformInputContextPlugin {
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QPlatformInputContextFactoryInterface_iid FILE "kime.json")
+
 private:
     InputEngine *engine = nullptr;
     Config *config = nullptr;
@@ -13,5 +18,5 @@ public:
     KimePlatformInputContextPlugin();
     ~KimePlatformInputContextPlugin();
     
-    QPlatformInputContext *create(const QString &key, const QStringList &param_list) override;
+    QPlatformInputContext *create(const QString &key, const QStringList &param_list) Q_DECL_OVERRIDE;
 };

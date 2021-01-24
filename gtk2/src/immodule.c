89eccac8595fc81938fcb333e72884a148733973
Riey <creeper844@gmail.com>
2021-01-24 18:51:30 +0900

    Add gtk4 frontend

--- gtk2/src/immodule.c
+++ gtk2/src/immodule.c
@@ -4,7 +4,19 @@
 
 static GType KIME_TYPE_IM_CONTEXT = 0;
 static const guint FORWARDED_MASK = 1 << 25;
-static const guint SKIP_MASK = GDK_MOD1_MASK | GDK_MOD3_MASK | GDK_MOD5_MASK;
+
+#if GTK_CHECK_VERSION(3, 98, 4)
+typedef GtkWidget ClientType;
+typedef GdkEvent EventType;
+#else
+#define GDK_ALT_MASK GDK_MOD1_MASK
+typedef GdkWindow ClientType;
+typedef GdkEventKey EventType;
+#endif
+
+static const guint SKIP_MASK = GDK_ALT_MASK;
+static const guint NOT_ENGLISH_MASK =
+    GDK_ALT_MASK | GDK_CONTROL_MASK | GDK_SUPER_MASK;
 
 typedef struct KimeSignals {
   guint commit;
@@ -21,7 +33,7 @@ typedef struct KimeImContextClass {
 
 typedef struct KimeImContext {
   GtkIMContext parent;
-  GdkWindow *client_window;
+  ClientType *client;
   KimeSignals signals;
   InputEngine *engine;
   gboolean preedit_visible;
@@ -69,17 +81,27 @@ void reset(GtkIMContext *im) {
   }
 }
 
-void put_event(GdkEventKey *key) {
+void put_event(KimeImContext *ctx, EventType *key) {
+#if GTK_CHECK_VERSION(3, 98, 4)
+  gtk_im_context_filter_key(
+    GTK_IM_CONTEXT(ctx),
+    gdk_event_get_event_type(key) == GDK_KEY_PRESS,
+    gdk_event_get_surface(key),
+    gdk_event_get_device(key),
+    gdk_event_get_time(key),
+    gdk_key_event_get_keycode(key),
+    gdk_event_get_modifier_state(key) | FORWARDED_MASK,
+    0
+  );
+#else
   key->state |= FORWARDED_MASK;
   gdk_event_put(gdk_event_copy((GdkEvent *)key));
+#endif
 }
 
-gboolean commit_event(KimeImContext *ctx, GdkEventKey *key) {
-  // ignore LOCK or NUMLOCK
-  guint state = key->state & !(GDK_LOCK_MASK | GDK_MOD2_MASK);
-
-  if (!state || state == GDK_SHIFT_MASK) {
-    uint32_t c = gdk_keyval_to_unicode(key->keyval);
+gboolean commit_event(KimeImContext *ctx, GdkModifierType state, guint keyval) {
+  if (!(state & NOT_ENGLISH_MASK)) {
+    uint32_t c = gdk_keyval_to_unicode(keyval);
 
     if (!g_unichar_iscntrl(c)) {
       commit(ctx, c);
@@ -90,46 +112,59 @@ gboolean commit_event(KimeImContext *ctx, GdkEventKey *key) {
   return FALSE;
 }
 
-gboolean bypass(KimeImContext *ctx, GdkEventKey *key) {
+gboolean bypass(KimeImContext *ctx, EventType *key) {
   uint32_t c = kime_engine_reset(ctx->engine);
 
   if (!c) {
     update_preedit(ctx, FALSE);
     commit(ctx, c);
-    put_event(key);
+    put_event(ctx, key);
     return TRUE;
   } else {
     return FALSE;
   }
 }
 
-gboolean filter_keypress(GtkIMContext *im, GdkEventKey *key) {
+gboolean filter_keypress(GtkIMContext *im, EventType *key) {
   KIME_IM_CONTEXT(im);
-
+#if GTK_CHECK_VERSION(3, 98, 4)
+  if (gdk_event_get_event_type(key) != GDK_KEY_PRESS) {
+    return FALSE;
+  }
+  guint16 code = gdk_key_event_get_keycode(key);
+  guint keyval = gdk_key_event_get_keyval(key);
+  GdkModifierType state = gdk_event_get_modifier_state(key);
+#else
   if (key->type != GDK_KEY_PRESS) {
     return FALSE;
-  } else if (key->state & FORWARDED_MASK) {
-    g_debug("Forwarded: %u", key->keyval);
-    return commit_event(ctx, key);
-  } else if (key->state & SKIP_MASK) {
+  }
+  guint16 code = key->hardware_keycode;
+  guint keyval = key->keyval;
+  GdkModifierType state = key->state;
+#endif
+
+  if (state & FORWARDED_MASK) {
+    g_debug("Forwarded: %u", keyval);
+    return commit_event(ctx, state, keyval);
+  } else if (state & SKIP_MASK) {
     return bypass(ctx, key);
   } else {
-    ModifierState state = 0;
+    ModifierState kime_state = 0;
 
-    if (key->state & GDK_SHIFT_MASK) {
-      state |= ModifierState_SHIFT;
+    if (state & GDK_SHIFT_MASK) {
+      kime_state |= ModifierState_SHIFT;
     }
 
-    if (key->state & GDK_CONTROL_MASK) {
-      state |= ModifierState_CONTROL;
+    if (state & GDK_CONTROL_MASK) {
+      kime_state |= ModifierState_CONTROL;
     }
 
-    if (key->state & GDK_MOD4_MASK) {
-      state |= ModifierState_SUPER;
+    if (state & GDK_SUPER_MASK) {
+      kime_state |= ModifierState_SUPER;
     }
 
-    InputResult ret = kime_engine_press_key(ctx->engine, ctx->config,
-                                            key->hardware_keycode, state);
+    InputResult ret =
+        kime_engine_press_key(ctx->engine, ctx->config, code, kime_state);
 
     switch (ret.ty) {
     case Commit:
@@ -145,8 +180,8 @@ gboolean filter_keypress(GtkIMContext *im, GdkEventKey *key) {
       update_preedit(ctx, FALSE);
       commit(ctx, ret.char1);
       // try commit english first
-      if (!commit_event(ctx, key)) {
-        put_event(key);
+      if (!commit_event(ctx, state, keyval)) {
+        put_event(ctx, key);
       }
       return TRUE;
     case CommitPreedit:
@@ -161,23 +196,23 @@ gboolean filter_keypress(GtkIMContext *im, GdkEventKey *key) {
       return TRUE;
     case Bypass:
     default:
-      return commit_event(ctx, key);
+      return commit_event(ctx, state, keyval);
     }
   }
 }
 
-void set_client_window(GtkIMContext *im, GdkWindow *window) {
+void set_client(GtkIMContext *im, ClientType *client) {
   KIME_IM_CONTEXT(im);
 
-  if (ctx->client_window) {
-    g_object_unref(ctx->client_window);
+  if (ctx->client) {
+    g_object_unref(ctx->client);
   }
 
-  if (window) {
-    g_object_ref(window);
+  if (client) {
+    g_object_ref(client);
   }
 
-  ctx->client_window = window;
+  ctx->client = client;
 }
 
 void get_preedit_string(GtkIMContext *im, gchar **out, PangoAttrList **attrs,
@@ -226,7 +261,7 @@ void im_context_class_finalize(KimeImContextClass *klass, gpointer _data) {
 }
 
 void im_context_init(KimeImContext *ctx, KimeImContextClass *klass) {
-  ctx->client_window = NULL;
+  ctx->client = NULL;
   ctx->signals = klass->signals;
   ctx->engine = kime_engine_new();
   ctx->config = klass->config;
@@ -248,7 +283,11 @@ void im_context_class_init(KimeImContextClass *klass, gpointer _data) {
 
   klass->config = kime_config_load();
 
-  klass->parent.set_client_window = set_client_window;
+#if GTK_CHECK_VERSION(3, 98, 4)
+  klass->parent.set_client_widget = set_client;
+#else
+  klass->parent.set_client_window = set_client;
+#endif
   klass->parent.reset = reset;
   klass->parent.filter_keypress = filter_keypress;
   klass->parent.get_preedit_string = get_preedit_string;

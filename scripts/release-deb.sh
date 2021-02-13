1d38fa18c251d2c7db2dd234a555ff1e4a05ff60
Riey <creeper844@gmail.com>
2021-02-14 02:47:33 +0900

    Ubuntu package (#242)
    * Install im-config entry
    
    * Set ubuntu docker
    
    * Update ci scripts
    
    * Workaround arch docker bug
    
    * Update CHANGELOG

--- scripts/release-deb.sh
+++ scripts/release-deb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+KIME_PREFIX=51_kime
+
 source $(dirname $0)/tool.sh
 
 if [ -z "$1" ]; then
@@ -12,10 +14,15 @@ TMP_PATH=$(mktemp -d)
 VER=$(git tag --sort=v:refname | tail -1 | cut -b2-)
 
 mkdir -pv $TMP_PATH/DEBIAN
+mkdir -pv $TMP_PATH/usr/share/im-config/data
 
 cat scripts/control.in | sed "s/%VER%/$VER/" > $TMP_PATH/DEBIAN/control
+cp scripts/im_kime.rc $TMP_PATH/usr/share/im-config/data/$KIME_PREFIX.rc
+cp scripts/im_kime.conf $TMP_PATH/usr/share/im-config/data/$KIME_PREFIX.conf
 
+KIME_LIB_DIR=usr/lib/x86_64-linux-gnu \
+KIME_QT5_DIR=usr/lib/x86_64-linux-gnu/qt5 \
+KIME_QT6_DIR=usr/lib/x86_64-linux-gnu/qt6 \
 scripts/install.sh $TMP_PATH
 
-dpkg-deb --build $TMP_PATH "${TARGET_PATH}/kime_${VER}_amd64.deb"
-
+dpkg-deb --build $TMP_PATH "${TARGET_PATH}/kime_amd64.deb"

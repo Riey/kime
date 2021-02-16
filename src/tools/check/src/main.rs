9e695b8003833ee5dc7544f434114f9e0f30b79e
Riey <creeper844@gmail.com>
2021-02-17 01:11:20 +0900

    Add Config check

--- src/tools/check/src/main.rs
+++ src/tools/check/src/main.rs
@@ -1,49 +1,45 @@
-use std::env;
-use strum::{EnumIter, IntoStaticStr, IntoEnumIterator, EnumMessage, EnumProperty};
 use ansi_term::Color;
+use std::env;
+use strum::{EnumIter, EnumMessage, IntoEnumIterator, IntoStaticStr};
 
-#[derive(Clone, Copy, PartialEq, Eq, IntoStaticStr)]
+#[derive(Clone, PartialEq, Eq, IntoStaticStr)]
 enum CondResult {
-    #[strum(to_string = "  (OK)  ")]
     Ok,
-    #[strum(to_string = " (FAIL) ")]
-    Fail,
-    #[strum(to_string = "(IGNORE)")]
-    Ignore,
+    Fail(String),
+    Ignore(String),
 }
 
 impl CondResult {
-    pub fn color(self) -> Color {
+    pub fn color(&self) -> Color {
         match self {
             CondResult::Ok => Color::Green,
-            CondResult::Fail => Color::Red,
-            CondResult::Ignore => Color::Purple,
+            CondResult::Fail(..) => Color::Red,
+            CondResult::Ignore(..) => Color::Purple,
+        }
+    }
+
+    pub fn print(&self, message: &str) {
+        let c = self.color();
+        print!("{:<6} {:<30}", c.paint(<&str>::from(self)), message);
+
+        match self {
+            CondResult::Ok => println!(),
+            CondResult::Fail(msg) | CondResult::Ignore(msg) => println!("({})", c.paint(msg)),
         }
     }
 }
 
-impl From<bool> for CondResult {
-    fn from(b: bool) -> Self {
-        match b {
-            true => Self::Ok,
-            false => Self::Fail,
-        }
-    }
-}
-
-#[derive(Clone, Copy, EnumIter, EnumMessage, EnumProperty)]
+#[derive(Clone, Copy, EnumIter, EnumMessage)]
 enum Check {
     #[strum(message = "Engine api version")]
-    #[strum(props(fail = "Install correct kime engine"))]
     ApiVersion,
+    #[strum(message = "Config file")]
+    Config,
     #[strum(message = "XMODIFIERS env")]
-    #[strum(props(fail = "Set XMODIFIERS=@im=kime", ignore = "Session is not x11"))]
     XModifier,
     #[strum(message = "GTK_IM_MODULE env")]
-    #[strum(props(fail = "Set GTK_IM_MODUILE=kime"))]
     GtkImModule,
     #[strum(message = "QT_IM_MODULE env")]
-    #[strum(props(fail = "Set QT_IM_MODULE=kime"))]
     QtImModule,
 }
 
@@ -51,47 +47,52 @@ impl Check {
     pub fn cond(self) -> CondResult {
         match self {
             Check::ApiVersion => {
-                kime_engine_cffi::check_api_version().into()
-            }
-            Check::XModifier => {
-                match env::var("XDG_SESSION_TYPE").unwrap().as_str() {
-                    "x11" => check_var("XMODIFIERS", "@im=kime"),
-                    _ => CondResult::Ignore,
+                if kime_engine_cffi::check_api_version() {
+                    CondResult::Ok
+                } else {
+                    CondResult::Fail("Install correct kime engine".into())
                 }
             }
-            Check::GtkImModule => {
-                check_var("GTK_IM_MODULE", "kime")
+            Check::Config => {
+                let dirs = xdg::BaseDirectories::with_prefix("kime").expect("Load xdg dirs");
+                let config_path = match dirs.find_config_file("config.yaml") {
+                    Some(path) => path,
+                    _ => return CondResult::Fail("Can't find config.yaml".into()),
+                };
+
+                println!("Loading config path: {}", config_path.display());
+
+                let _config: kime_engine_core::RawConfig = match serde_yaml::from_str(&std::fs::read_to_string(config_path).expect("Read config file")) {
+                    Ok(config) => config,
+                    Err(err) => return CondResult::Fail(format!("Can't parse config.yaml: {}", err))
+                };
+
+                // TODO: check layout
+
+                CondResult::Ok
             }
-            Check::QtImModule => {
-                check_var("QT_IM_MODULE", "kime")
-            }
+            Check::XModifier => match env::var("XDG_SESSION_TYPE").unwrap().as_str() {
+                "x11" => check_var("XMODIFIERS", "@im=kime", "set XMODIFIERS=@im=kime"),
+                other => CondResult::Ignore(format!("Session type is {} not x11", other)),
+            },
+            Check::GtkImModule => check_var("GTK_IM_MODULE", "kime", "set GTK_IM_MODULE=kime"),
+            Check::QtImModule => check_var("QT_IM_MODULE", "kime", "set QT_IM_MODULE=kime"),
         }
     }
 }
 
-fn check_var(name: &str, value: &str) -> CondResult {
-    env::var(name).map_or(false, |v| v.contains(value)).into()
+fn check_var(name: &str, value: &str, reason: &str) -> CondResult {
+    if env::var(name).map_or(false, |v| v.contains(value)) {
+        CondResult::Ok
+    } else {
+        CondResult::Fail(reason.into())
+    }
 }
 
 fn main() {
     for check in Check::iter() {
         let ret = check.cond();
-        let c = ret.color();
 
-        print!("{} {:<30}", c.paint(<&str>::from(ret)), check.get_message().unwrap());
-
-        match ret {
-            CondResult::Ok => println!(),
-            CondResult::Fail => {
-                if let Some(fail) = check.get_str("fail") {
-                    println!(" ({})", c.paint(fail));
-                }
-            }
-            CondResult::Ignore => {
-                if let Some(ignore) = check.get_str("ignore") {
-                    println!(" ({})", c.paint(ignore));
-                }
-            }
-        }
+        ret.print(check.get_message().unwrap());
     }
 }

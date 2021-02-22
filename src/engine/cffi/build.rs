fcb0db63c964ec78479676fee3fd3f4bfd0fde5c
Riey <creeper844@gmail.com>
2021-02-22 14:47:51 +0900

    Make engine-cffi no_std (#290)
    * Make engine-cffi no_std
    
    * Make rerun build script when api changed
    
    * Update release workflow
    
    * Remove update_bindings.sh

---
+++ src/engine/cffi/build.rs
@@ -0,0 +1,39 @@
+use std::env;
+
+fn main() {
+    println!("cargo:rerun-if-changed=./kime_engine.h");
+    println!("cargo:rerun-if-changed=./kime_engine.hpp");
+    println!("cargo:rerun-if-changed=../capi");
+
+    let c_binding = cbindgen::generate_with_config(
+        "../capi",
+        cbindgen::Config::from_file("../capi/cbindgen-c.toml").unwrap(),
+    )
+    .unwrap();
+
+    c_binding.write_to_file("kime_engine.h");
+
+    let cpp_binding = cbindgen::generate_with_config(
+        "../capi",
+        cbindgen::Config::from_file("../capi/cbindgen-cpp.toml").unwrap(),
+    )
+    .unwrap();
+
+    cpp_binding.write_to_file("kime_engine.hpp");
+
+    let rust_binding = bindgen::builder()
+        .header("./kime_engine.hpp")
+        .disable_name_namespacing()
+        .whitelist_var("kime::.+")
+        .whitelist_type("kime::.+")
+        .whitelist_function("kime::.+")
+        .use_core()
+        .ctypes_prefix("cty")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .unwrap();
+
+    rust_binding
+        .write_to_file(std::path::PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
+        .unwrap();
+}

b8723768995e51ec98a8708140d97c0f651d31c4
Riey <creeper844@gmail.com>
2021-03-19 13:36:17 +0900

    Fix can't select hanja (#382)
    * Update bench
    
    * Don't use category_hotkey when mode is enabled

--- src/engine/cffi/benches/call_key.rs
+++ src/engine/cffi/benches/call_key.rs
@@ -11,13 +11,14 @@ fn simple(b: &mut test::Bencher) {
 
     let config = Config::default();
     let mut engine = InputEngine::new(&config);
-    engine.set_input_category(&config, InputCategory::Hangul);
+    engine.set_input_category(InputCategory::Hangul);
 
     engine.press_key(&config, 52, 0);
 
     b.iter(|| {
         for _ in 0..1000 {
             engine.press_key(&config, 52, 0);
+            engine.clear_commit();
         }
     });
 }

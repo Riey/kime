7715285ebdcb864b49b66398c3c011844343e342
Riey <creeper844@gmail.com>
2021-04-08 20:41:29 +0900

    Caching /nix/store

--- default.nix
+++ default.nix
@@ -9,50 +9,13 @@
 with pkgs;
 let
   kimeVersion = builtins.readFile ./VERSION;
-  kimeBuildInputs = [
-    dbus
-    dbus_libs
-    libdbusmenu
-
-    xorg.libxcb
-    xlibs.libpthreadstubs
-    xlibs.libXdmcp.dev
-
-    cairo
-    pcre
-
-    glib
-    libselinux.dev
-    libsepol.dev
-    utillinux.dev
-    gtk2
-    gtk3
-    at_spi2_core.dev
-    epoxy.dev
-    xlibs.libXtst
-
-    # gtk4
-
-    qt5.qtbase
-    # qt6.qtbase
-  ];
-  kimeNativeBuildInputs = [
-    bash
-    pkg-config
-    clang_11
-    qt5.wrapQtAppsHook
-    llvmPackages_11.libclang
-    llvmPackages_11.bintools
-    rustc cargo
-    cmake
-    extra-cmake-modules
-  ];
+  deps = import ./deps.nix { pkgs = pkgs; };
 in
 rustPlatform.buildRustPackage rec {
   src = gis.gitIgnoreSource ./.;
   pname = "kime";
-  buildInputs = kimeBuildInputs;
-  nativeBuildInputs = kimeNativeBuildInputs;
+  buildInputs = deps.kimeBuildInputs;
+  nativeBuildInputs = deps.kimeNativeBuildInputs;
   version = kimeVersion;
   cargoSha256 = "146m1kg83bmlf6sxi20yawksp47qp0byrh07wkv56vyl5p02fsz1";
   LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang}/lib";

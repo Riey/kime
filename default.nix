0091495cbf5dc63c7567488a0758237120b91a08
Riey <creeper844@gmail.com>
2021-04-08 19:43:16 +0900

    Retry nix ci

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@
     url = https://github.com/icetan/nix-git-ignore-source/archive/v1.0.0.tar.gz;
     sha256 = "1mnpab6x0bnshpp0acddylpa3dslhzd2m1kk3n0k23jqf9ddz57k";
   }) {},
+  debug ? false,
 }:
 with pkgs;
 let
@@ -56,7 +57,8 @@ rustPlatform.buildRustPackage rec {
   cargoSha256 = "146m1kg83bmlf6sxi20yawksp47qp0byrh07wkv56vyl5p02fsz1";
   LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang}/lib";
   dontUseCmakeConfigure = true;
-  buildPhase = "bash scripts/build.sh -ar";
+  checkType = if debug then "debug" else "release";
+  buildPhase = if debug then "bash scripts/build.sh -ad" else "bash scripts/build.sh -ar";
   installPhase = ''
     KIME_BIN_DIR=bin \
     KIME_INSTALL_HEADER=1 \

4769f76d00947edf9f2247e6049944ad890ac77b
Riey <creeper844@gmail.com>
2021-04-08 16:28:45 +0900

    Update tools

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 {
-  pkgs ? import <nixpkgs> { },
+  moz_overlay ? import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz),
+  pkgs ? import <nixpkgs> { overlays = [ moz_overlay ]; },
 }:
 with pkgs;
+let
+  rust-toolchain = pkgs.rustChannelOf { version = "1.51.0"; channel = "stable"; };
+in
 llvmPackages_11.stdenv.mkDerivation {
   name = "kime-shell";
 
@@ -41,14 +45,14 @@ llvmPackages_11.stdenv.mkDerivation {
     clang_11
     llvmPackages_11.libclang
     llvmPackages_11.bintools
-    cargo
-    rustc
     dpkg
     gnutar
     zstd
     cmake
     extra-cmake-modules
     qt5.wrapQtAppsHook
+
+    rust-toolchain.rust
   ];
   LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang}/lib";
 }

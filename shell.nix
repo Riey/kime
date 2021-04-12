1208ca29614df24a3d6f06a87559eee8af65ed2f
Riey <creeper844@gmail.com>
2021-04-12 16:00:40 +0900

    Fix cargo-deny

--- shell.nix
+++ shell.nix
@@ -15,7 +15,11 @@ llvmPackages_11.stdenv.mkDerivation {
   buildInputs = deps.kimeBuildInputs;
   nativeBuildInputs = deps.kimeNativeBuildInputs
                       ++ lib.optionals packaging [ gnutar zstd dpkg ]
-                      ++ lib.optionals develop [ rust-analyzer ];
+                      ++ lib.optionals develop [
+                        rust-analyzer
+                        cargo-deny
+                        cargo-outdated
+                      ];
   CMAKE_EXPORT_COMPILE_COMMANDS = 1;
   LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang}/lib";
   RUST_BACKTRACE = 1;

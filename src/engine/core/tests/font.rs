4dac963457866384bc9bda838f80f6bd39547316
Riey <creeper844@gmail.com>
2021-11-24 23:18:05 +0900

    Remove invalid test

--- src/engine/core/tests/font.rs
+++
@@ -1,7 +0,0 @@
-use kime_engine_core::load_engine_config_from_config_dir;
-
-#[test]
-fn font() {
-    let config = load_engine_config_from_config_dir().unwrap();
-    assert!(!config.candidate_font.0.is_empty());
-}
